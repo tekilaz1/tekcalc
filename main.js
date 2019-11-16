ea70d6c301f5c28ac14c8e326c7265c8d240dc3a
tekilaz1 <tselama18@gmail.com>
2019-11-16 10:14:09 +0200

    some changes except the eva
    Signed-off-by: tekilaz1 <tselama18@gmail.com>

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function back(){
 
-                   var bac= document.cal.display.value;
+                   var bac = document.cal.display.value;
                    document.cal.display.value = bac.substring(0,bac.length-1);
 }

64af12720daf4f05249c3512959a19a0dbab29c3
tekilaz1 <tselama18@gmail.com>
2019-11-23 12:03:39 +0200

    updated to be scientfic
    Signed-off-by: tekilaz1 <tselama18@gmail.com>

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ function back(){
                    var bac = document.cal.display.value;
                    document.cal.display.value = bac.substring(0,bac.length-1);
 }
+
+// function tesfie(dis){
+
+// 	     var dis = document.cal.display.value;
+	
+//             document.cal.display.value = Math.evalaute(dis)
+//         }
